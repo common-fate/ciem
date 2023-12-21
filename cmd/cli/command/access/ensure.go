98a88f90124d372ce931c6488b233751dc4208c3
Josh Wilkes <14214200+JoshuaWilkes@users.noreply.github.com>
2023-12-21 13:41:28 +0800

    POC table based selector UI

--- cmd/cli/command/access/ensure.go
+++ cmd/cli/command/access/ensure.go
@@ -1,32 +1,204 @@
 package access
 
 import (
+	"context"
 	"errors"
 	"fmt"
 	"net/url"
 	"os"
+	"slices"
+	"strconv"
 	"strings"
 	"time"
 
 	"github.com/briandowns/spinner"
 	"github.com/bufbuild/connect-go"
+	"github.com/charmbracelet/bubbles/textinput"
+	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/lipgloss"
+	"github.com/common-fate/ciem/multiselecttable"
 	"github.com/common-fate/ciem/printdiags"
 	"github.com/common-fate/clio"
+	"github.com/common-fate/grab"
 	"github.com/common-fate/sdk/config"
 	"github.com/common-fate/sdk/eid"
 	accessv1alpha1 "github.com/common-fate/sdk/gen/commonfate/access/v1alpha1"
 	"github.com/common-fate/sdk/service/access"
 	"github.com/fatih/color"
+	"github.com/lithammer/fuzzysearch/fuzzy"
 	"github.com/urfave/cli/v2"
 	"google.golang.org/protobuf/encoding/protojson"
 )
+
+var baseStyle = lipgloss.NewStyle().
+	BorderStyle(lipgloss.NormalBorder()).
+	BorderForeground(lipgloss.Color("#ED77C0"))
+
+type model struct {
+	filter textinput.Model
+	table  multiselecttable.Model
+
+	rows         []multiselecttable.Row
+	filteredRows []multiselecttable.Row
+}
+
+type resetSelectedMsg struct {
+}
+
+func (m model) Init() tea.Cmd {
+	return tea.Batch(func() tea.Msg {
+		return resetSelectedMsg{}
+	}, textinput.Blink)
+
+}
+
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	var tableCmd tea.Cmd
+	var filterCmd tea.Cmd
+	switch msg := msg.(type) {
+	case resetSelectedMsg:
+		m.table.Selected = make(map[int]bool)
+	case tea.KeyMsg:
+		switch msg.String() {
+		case "esc":
+			if m.table.Focused() {
+				m.table.Blur()
+			} else {
+				m.table.Focus()
+			}
+		case "q", "ctrl+c":
+			return m, tea.Quit
+		case "enter":
+			m.table.Selected[m.table.Cursor()] = !m.table.Selected[m.table.Cursor()]
+		case "ctrl+e":
+			return m, tea.Quit
+		case "down", "up":
+
+			// prevents the filter input responding to up down
+			m.table, tableCmd = m.table.Update(msg)
+			return m, tableCmd
+		}
+	}
+	trimmed := strings.Trim(m.filter.Value(), " ")
+	m.filteredRows = []multiselecttable.Row{}
+	if trimmed == "" {
+		m.table.SetRows(m.rows)
+	} else {
+		filters := slices.Compact(strings.Split(trimmed, " "))
+
+		// fuzzy match on any colum in the row
+	Row:
+		for i, r := range m.rows {
+			for _, v := range r {
+				for _, f := range filters {
+					if fuzzy.Match(f, v) {
+						m.filteredRows = append(m.filteredRows, m.rows[i])
+						continue Row
+					}
+				}
+
+			}
+		}
+		m.table.SetRows(m.filteredRows)
+	}
+
+	m.table, tableCmd = m.table.Update(msg)
+	m.filter, filterCmd = m.filter.Update(msg)
+	return m, tea.Batch(tableCmd, filterCmd)
+}
+
+func (m model) View() string {
+	return baseStyle.Render(m.filter.View(), "\n\n", m.table.View(), "\n"+"(ctrl+e to confirm selection. ctrl+c to quit)") + "\n"
+}
+func selector(ctx context.Context) ([]*accessv1alpha1.Availability, error) {
+
+	cfg, err := config.LoadDefault(ctx)
+	if err != nil {
+		return nil, err
+	}
+	client := access.NewFromConfig(cfg)
+
+	availabilities, err := grab.AllPages(ctx, func(ctx context.Context, nextToken *string) ([]*accessv1alpha1.Availability, *string, error) {
+		res, err := client.QueryAvailabilities(ctx, connect.NewRequest(&accessv1alpha1.QueryAvailabilitiesRequest{
+			PageToken: grab.Value(nextToken),
+		}))
+		if err != nil {
+			return nil, nil, err
+		}
+		return res.Msg.Availabilities, &res.Msg.NextPageToken, nil
+	})
+	if err != nil {
+		return nil, err
+	}
+	_ = availabilities
+
+	columns := []multiselecttable.Column{
+		{Title: "TARGET", Width: 30},
+		{Title: "NAME", Width: 45},
+		{Title: "ROLE", Width: 30},
+		{Title: "DURATION", Width: 10},
+		{Title: "SELECTOR", Width: 20},
+		{Title: "PRIORITY", Width: 10},
+	}
+	rows := []multiselecttable.Row{}
+
+	var suggestions []string
+	for _, a := range availabilities {
+		rows = append(rows, []string{a.Target.Eid.Display(), a.Target.Name, a.Role.Name, a.Duration.AsDuration().String(), a.TargetSelector.Id, strconv.FormatUint(uint64(a.Priority), 10)})
+		suggestions = append(suggestions, a.Target.Eid.Display(), a.Target.Name, a.Role.Name)
+	}
+	t := multiselecttable.New(
+		multiselecttable.WithColumns(columns),
+		multiselecttable.WithRows(rows),
+		multiselecttable.WithFocused(true),
+		multiselecttable.WithHeight(7),
+	)
+
+	s := multiselecttable.DefaultStyles()
+
+	s.Header = s.Header.
+		BorderStyle(lipgloss.NormalBorder()).
+		BorderForeground(lipgloss.Color("#ED77C0")).
+		BorderBottom(true).
+		Bold(false).Foreground(lipgloss.Color("#619EFF"))
+	s.Selected = s.Selected.
+		Foreground(lipgloss.Color("#000000")).
+		Background(lipgloss.Color("#30D15D")).
+		Bold(false)
+	s.Highlighted = s.Highlighted.
+		Foreground(lipgloss.Color("#000000")).
+		Background(lipgloss.Color("#30D15D")).
+		Bold(true)
+
+	t.SetStyles(s)
+	ti := textinput.New()
+	ti.Placeholder = "Filter by target or role, use spaces to search by multiple terms"
+	ti.Focus()
+	ti.CharLimit = 156
+	ti.Width = 100
+	ti.SetSuggestions(suggestions)
+
+	ti.ShowSuggestions = true
+	m := model{table: t, filter: ti, rows: rows}
+	final, err := tea.NewProgram(m).Run()
+	if err != nil {
+		return nil, err
+	}
+
+	m = final.(model)
+	var result []*accessv1alpha1.Availability
+	for i := range m.table.Selected {
+		result = append(result, availabilities[i])
+	}
+	return result, nil
+}
 
 var ensureCommand = cli.Command{
 	Name:  "ensure",
 	Usage: "Ensure access to some entitlements (will request, active, or extend access as necessary)",
 	Flags: []cli.Flag{
-		&cli.StringSliceFlag{Name: "target", Required: true},
-		&cli.StringSliceFlag{Name: "role", Required: true},
+		&cli.StringSliceFlag{Name: "target", Required: false},
+		&cli.StringSliceFlag{Name: "role", Required: false},
 		&cli.StringFlag{Name: "output", Value: "text", Usage: "output format ('text' or 'json')"},
 		&cli.BoolFlag{Name: "confirm", Aliases: []string{"y"}, Usage: "skip the confirmation prompt"},
 	},
@@ -52,29 +224,55 @@ var ensureCommand = cli.Command{
 			return errors.New("you need to provide --role flag for each --target flag. For example:\n'cf access ensure --target AWS::Account::123456789012 --role AdministratorAccess --target OtherAccount --role Developer")
 		}
 
+		req := accessv1alpha1.BatchEnsureRequest{}
+
+		if len(targets) == 0 {
+			selections, err := selector(ctx)
+			if err != nil {
+				return err
+			}
+
+			if len(selections) == 0 {
+				clio.Info("No availabilities selected")
+				return nil
+			}
+			for _, selection := range selections {
+				req.Entitlements = append(req.Entitlements, &accessv1alpha1.EntitlementInput{
+					Target: &accessv1alpha1.Specifier{
+						Specify: &accessv1alpha1.Specifier_Lookup{
+							Lookup: selection.Target.Name,
+						},
+					},
+					Role: &accessv1alpha1.Specifier{
+						Specify: &accessv1alpha1.Specifier_Lookup{
+							Lookup: selection.Role.Name,
+						},
+					},
+				})
+			}
+		} else {
+			for i, target := range targets {
+				req.Entitlements = append(req.Entitlements, &accessv1alpha1.EntitlementInput{
+					Target: &accessv1alpha1.Specifier{
+						Specify: &accessv1alpha1.Specifier_Lookup{
+							Lookup: target,
+						},
+					},
+					Role: &accessv1alpha1.Specifier{
+						Specify: &accessv1alpha1.Specifier_Lookup{
+							Lookup: roles[i],
+						},
+					},
+				})
+			}
+		}
+
 		apiURL, err := url.Parse(cfg.APIURL)
 		if err != nil {
 			return err
 		}
 
 		client := access.NewFromConfig(cfg)
-
-		req := accessv1alpha1.BatchEnsureRequest{}
-
-		for i, target := range targets {
-			req.Entitlements = append(req.Entitlements, &accessv1alpha1.EntitlementInput{
-				Target: &accessv1alpha1.Specifier{
-					Specify: &accessv1alpha1.Specifier_Lookup{
-						Lookup: target,
-					},
-				},
-				Role: &accessv1alpha1.Specifier{
-					Specify: &accessv1alpha1.Specifier_Lookup{
-						Lookup: roles[i],
-					},
-				},
-			})
-		}
 
 		if !c.Bool("confirm") {
 			jsonOutput := c.String("output") == "json"
--- go.mod
+++ go.mod
@@ -6,10 +6,15 @@ require (
 	github.com/AlecAivazis/survey/v2 v2.3.7
 	github.com/briandowns/spinner v1.23.0
 	github.com/bufbuild/connect-go v1.10.0
+	github.com/charmbracelet/bubbles v0.17.1
+	github.com/charmbracelet/bubbletea v0.25.0
+	github.com/charmbracelet/lipgloss v0.9.1
 	github.com/common-fate/clio v1.2.3
+	github.com/common-fate/grab v1.2.0
 	github.com/common-fate/sdk v0.0.0-20231220055240-32a937c74bf1
 	github.com/fatih/color v1.16.0
 	github.com/mattn/go-isatty v0.0.20
+	github.com/mattn/go-runewidth v0.0.15
 	github.com/pkg/errors v0.9.1
 	github.com/stretchr/testify v1.8.4
 	github.com/urfave/cli/v2 v2.25.7
@@ -22,8 +27,10 @@ require (
 	github.com/99designs/go-keychain v0.0.0-20191008050251-8e49817e8af4 // indirect
 	github.com/99designs/keyring v1.2.2 // indirect
 	github.com/BurntSushi/toml v1.3.2 // indirect
+	github.com/atotto/clipboard v0.1.4 // indirect
+	github.com/aymanbagabas/go-osc52/v2 v2.0.1 // indirect
 	github.com/common-fate/apikit v0.3.0 // indirect
-	github.com/common-fate/grab v1.2.0 // indirect
+	github.com/containerd/console v1.0.4-0.20230313162750-1ae8d489ac81 // indirect
 	github.com/cpuguy83/go-md2man/v2 v2.0.2 // indirect
 	github.com/danieljoos/wincred v1.1.2 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
@@ -37,13 +44,21 @@ require (
 	github.com/gorilla/securecookie v1.1.1 // indirect
 	github.com/gsterjov/go-libsecret v0.0.0-20161001094733-a6f4afe4910c // indirect
 	github.com/kballard/go-shellquote v0.0.0-20180428030007-95032a82bc51 // indirect
+	github.com/lithammer/fuzzysearch v1.1.8
+	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-localereader v0.0.1 // indirect
 	github.com/mgutz/ansi v0.0.0-20170206155736-9520e82c474b // indirect
 	github.com/mtibben/percent v0.2.1 // indirect
+	github.com/muesli/ansi v0.0.0-20211018074035-2e021307bc4b // indirect
+	github.com/muesli/cancelreader v0.2.2 // indirect
+	github.com/muesli/reflow v0.3.0 // indirect
+	github.com/muesli/termenv v0.15.2 // indirect
 	github.com/muhlemmer/gu v0.3.1 // indirect
 	github.com/patrickmn/go-cache v2.1.0+incompatible // indirect
 	github.com/pkg/browser v0.0.0-20210911075715-681adbf594b8 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/rivo/uniseg v0.2.0 // indirect
 	github.com/russross/blackfriday/v2 v2.1.0 // indirect
 	github.com/xrash/smetrics v0.0.0-20201216005158-039620a65673 // indirect
 	github.com/zitadel/oidc/v2 v2.12.0 // indirect
