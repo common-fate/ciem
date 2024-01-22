bcc41ff299baaf6344a985b26e83cbff7100e238
Josh Wilkes <14214200+JoshuaWilkes@users.noreply.github.com>
2024-01-22 13:38:05 +0800

    add check for cli tools, call granted directly

--- cmd/cli/command/aws/rds/rds.go
+++ cmd/cli/command/aws/rds/rds.go
@@ -2,25 +2,24 @@ package rds
 
 import (
 	"context"
+	"encoding/json"
 	"errors"
 	"fmt"
 	"net/url"
 	"os"
 	"os/exec"
-	"os/signal"
 	"strconv"
 	"strings"
-	"syscall"
 	"time"
 
+	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/briandowns/spinner"
 	"github.com/bufbuild/connect-go"
 	accessCmd "github.com/common-fate/ciem/cmd/cli/command/access"
 	"github.com/common-fate/ciem/printdiags"
 	"github.com/common-fate/clio"
+	"github.com/common-fate/clio/clierr"
 	"github.com/common-fate/grab"
-	"github.com/common-fate/granted/pkg/assume"
-	"github.com/common-fate/granted/pkg/cfaws"
 	"github.com/fatih/color"
 
 	"github.com/common-fate/sdk/config"
@@ -31,8 +30,6 @@ import (
 	"github.com/common-fate/sdk/service/access/grants"
 	"github.com/common-fate/sdk/service/entity"
 	"github.com/urfave/cli/v2"
-
-	awsConfig "github.com/aws/aws-sdk-go-v2/config"
 )
 
 var Command = cli.Command{
@@ -58,6 +55,13 @@ var proxyCommand = cli.Command{
 		if err != nil {
 			return err
 		}
+
+		// ensure required CLI tools are installed
+		err = CheckDependencies()
+		if err != nil {
+			return err
+		}
+
 		target := c.String("target")
 
 		client := access.NewFromConfig(cfg)
@@ -175,45 +179,6 @@ var proxyCommand = cli.Command{
 		}
 
 		grantsClient := grants.NewFromConfig(cfg)
-		// idClient := identity.NewFromConfig(cfg)
-
-		// idRes, err := idClient.GetCallerIdentity(ctx, connect.NewRequest(&accessv1alpha1.GetCallerIdentityRequest{}))
-		// if err != nil {
-		// 	return err
-		// }
-
-		// matchedGrants, err := grab.AllPages(ctx, func(ctx context.Context, nextToken *string) ([]*accessv1alpha1.Grant, *string, error) {
-		// 	res, err := grantsClient.QueryGrants(ctx, connect.NewRequest(&accessv1alpha1.QueryGrantsRequest{
-		// 		PageToken: grab.Value(nextToken),
-		// 		Principal: idRes.Msg.Principal.Eid,
-		// 		Target: &entityv1alpha1.EID{
-		// 			Type: "AWS::RDS::Instance",
-		// 			Id:   target,
-		// 		},
-		// 		Role: &entityv1alpha1.EID{
-		// 			Type: "CF::Database::Role",
-		// 			Id:   "ReadWrite",
-		// 		},
-		// 		Status: accessv1alpha1.GrantStatus_GRANT_STATUS_ACTIVE.Enum(),
-		// 	}))
-		// 	if err != nil {
-		// 		return nil, nil, err
-		// 	}
-
-		// 	return res.Msg.Grants, &res.Msg.NextPageToken, nil
-		// })
-		// if err != nil {
-		// 	return err
-		// }
-
-		// if len(matchedGrants) == 0 {
-		// 	return errors.New("no matching grants found")
-		// }
-		// if len(matchedGrants) != 1 {
-		// 	clio.Debug("found more than one matching grant")
-		// }
-
-		// grant := matchedGrants[0]
 
 		children, err := grab.AllPages(ctx, func(ctx context.Context, nextToken *string) ([]*entityv1alpha1.Entity, *string, error) {
 			res, err := grantsClient.QueryGrantChildren(ctx, connect.NewRequest(&accessv1alpha1.QueryGrantChildrenRequest{
@@ -234,102 +199,194 @@ var proxyCommand = cli.Command{
 		}
 
 		for _, child := range children {
-			switch child.Eid.Type {
-			case "CF::Integration::AWSRDS::Grant::BastionInstance":
-				err = entity.Unmarshal(child, &commandData.BastionInstance)
+			if child.Eid.Type == "CF::GrantOutput::AWSRDS" {
+				err = entity.Unmarshal(child, &commandData.GrantOutput)
 				if err != nil {
 					return err
 				}
-			case "CF::Integration::AWSRDS::Grant::AccountAssignment":
-				err = entity.Unmarshal(child, &commandData.AccountAssignment)
-				if err != nil {
-					return err
-				}
-			default:
-				clio.Debugf("found unexpected child entity type %s", child.Eid.Type)
 			}
 		}
 
-		if commandData.BastionInstance.ID == "" {
-			return errors.New("did not find a bastion host in query grant children response")
+		if commandData.GrantOutput.Grant.ID == "" {
+			return errors.New("did not find a grant output entity in query grant children response")
 		}
-		if commandData.AccountAssignment.ID == "" {
-			return errors.New("did not find an account assignment in query grant children response")
-		}
-
-		sso := cfaws.AwsSsoAssumer{}
-		profile := &cfaws.Profile{
-			Name:        commandData.AccountAssignment.Grant.ID,
-			ProfileType: sso.Type(),
-			AWSConfig: awsConfig.SharedConfig{
-				SSOAccountID: commandData.AccountAssignment.Account.ID,
-				SSORoleName:  commandData.AccountAssignment.Grant.ID,
-				SSORegion:    commandData.AccountAssignment.IDCRegion,
-				SSOStartURL:  commandData.AccountAssignment.StartURL,
-			},
-			Initialised: true,
-		}
-
-		creds, err := profile.SSOLogin(ctx, cfaws.ConfigOpts{})
+		creds, err := GrantedCredentialProcess(commandData)
 		if err != nil {
 			return err
 		}
+		_ = creds
+		// sso := cfaws.AwsSsoAssumer{}
+		// profile := &cfaws.Profile{
+		// 	Name:        commandData.GrantOutput.Grant.ID,
+		// 	ProfileType: sso.Type(),
+		// 	AWSConfig: awsConfig.SharedConfig{
 
-		clio.Infof("starting database proxy on port %v", commandData.LocalPort)
-		cmd := exec.Command("aws", formatSSMCommandArgs(commandData)...)
-		clio.Debugw("running aws ssm command", "command", "assume "+strings.Join(formatSSMCommandArgs(commandData), " "))
-		cmd.Stderr = os.Stderr
-		cmd.Stdout = os.Stdout
-		cmd.Stdin = os.Stdin
-		cmd.Env = os.Environ()
-		cmd.Env = append(cmd.Env, assume.EnvKeys(creds, commandData.BastionInstance.Region)...)
+		// 		// I have opted to use a named SSO session block here to force a seperate sso login item to be added to the keychain.
+		// 		// due to keychain permissions, if you use the same sso token path as granted cli, it will break the permissions for granted
+		// 		// granted will then lose access to read and write the token and it needs to be deleted manaually via the keychain app
+		// 		// Named sessions usually can be used when you want to login as 2 seperate sso users for teh same start url.
+		// 		// we can use it here to create a seperate entry in the keychain just for the cf cli
+		// 		SSOSessionName: "cf-cli",
+		// 		SSOSession: &awsConfig.SSOSession{
+		// 			Name:        "cf-cli",
+		// 			SSORegion:   commandData.GrantOutput.SSORegion,
+		// 			SSOStartURL: commandData.GrantOutput.SSOStartURL,
+		// 		},
+		// 		SSOAccountID: commandData.GrantOutput.AccountID,
+		// 		SSORoleName:  commandData.GrantOutput.SSORoleName,
+		// 	},
+		// 	Initialised: true,
+		// }
+		// creds, err := profile.SSOLogin(ctx, cfaws.ConfigOpts{ShouldRetryAssuming: grab.Ptr(true)})
+		// if err != nil {
+		// 	return err
+		// }
 
-		// Start the command in a separate goroutine
-		err = cmd.Start()
-		if err != nil {
-			return err
-		}
+		// clio.Infof("starting database proxy on port %v", commandData.LocalPort)
+		// cmd := exec.Command("aws", formatSSMCommandArgs(commandData)...)
+		// clio.Debugw("running aws ssm command", "command", "assume "+strings.Join(formatSSMCommandArgs(commandData), " "))
+		// cmd.Stderr = os.Stderr
+		// cmd.Stdout = os.Stdout
+		// cmd.Stdin = os.Stdin
+		// cmd.Env = os.Environ()
+		// cmd.Env = append(cmd.Env, assume.EnvKeys(creds, commandData.GrantOutput.InstanceRegion)...)
 
-		// Set up a channel to receive OS signals
-		sigs := make(chan os.Signal, 1)
-		// Notify sigs on os.Interrupt (Ctrl+C)
-		signal.Notify(sigs, os.Interrupt, syscall.SIGTERM)
+		// // Start the command in a separate goroutine
+		// err = cmd.Start()
+		// if err != nil {
+		// 	return err
+		// }
 
-		// Wait for a termination signal in a separate goroutine
-		go func() {
-			<-sigs
-			clio.Info("Received interrupt signal, shutting down...")
-			if err := cmd.Process.Signal(os.Interrupt); err != nil {
-				clio.Error("Error sending SIGTERM to process:", err)
-			}
-		}()
+		// // Set up a channel to receive OS signals
+		// sigs := make(chan os.Signal, 1)
+		// // Notify sigs on os.Interrupt (Ctrl+C)
+		// signal.Notify(sigs, os.Interrupt, syscall.SIGTERM)
 
-		// Wait for the command to finish
-		err = cmd.Wait()
-		if err != nil {
-			clio.Error("Proxy connection failed with error:", err)
-		} else {
-			clio.Info("Proxy connection closed successfully")
-		}
+		// // Wait for a termination signal in a separate goroutine
+		// go func() {
+		// 	<-sigs
+		// 	clio.Info("Received interrupt signal, shutting down...")
+		// 	if err := cmd.Process.Signal(os.Interrupt); err != nil {
+		// 		clio.Error("Error sending SIGTERM to process:", err)
+		// 	}
+		// }()
+
+		// // Wait for the command to finish
+		// err = cmd.Wait()
+		// if err != nil {
+		// 	clio.Error("Proxy connection failed with error:", err)
+		// } else {
+		// 	clio.Info("Proxy connection closed successfully")
+		// }
 		return nil
 	},
 }
 
 type CommandData struct {
-	BastionInstance   BastionInstance
-	AccountAssignment AccountAssignment
-	LocalPort         string
+	GrantOutput GrantOutput
+	LocalPort   string
 }
 
 func formatSSMCommandArgs(data CommandData) []string {
 	out := []string{
 		"ssm",
 		"start-session",
-		fmt.Sprintf("--target=%s", data.BastionInstance.ID),
+		fmt.Sprintf("--target=%s", data.GrantOutput.InstanceID),
 		"--document-name=AWS-StartPortForwardingSession",
 		"--parameters",
 		fmt.Sprintf(`{"portNumber":["5432"], "localPortNumber":["%s"]}`, data.LocalPort),
 	}
 
 	return out
+}
+
+// CredentialProcessOutput represents the JSON output format of the credential process.
+type CredentialProcessOutput struct {
+	Version         int       `json:"Version"`
+	AccessKeyId     string    `json:"AccessKeyId"`
+	SecretAccessKey string    `json:"SecretAccessKey"`
+	SessionToken    string    `json:"SessionToken,omitempty"`
+	Expiration      time.Time `json:"Expiration,omitempty"`
+}
+
+// ParseCredentialProcessOutput parses the JSON output of a credential process and returns aws.Credentials.
+func ParseCredentialProcessOutput(credentialProcessOutput string) (aws.Credentials, error) {
+	var output CredentialProcessOutput
+	err := json.Unmarshal([]byte(credentialProcessOutput), &output)
+	if err != nil {
+		return aws.Credentials{}, fmt.Errorf("error parsing credential process output: %w", err)
+	}
+
+	return aws.Credentials{
+		AccessKeyID:     output.AccessKeyId,
+		SecretAccessKey: output.SecretAccessKey,
+		SessionToken:    output.SessionToken,
+		CanExpire:       !output.Expiration.IsZero(),
+		Expires:         output.Expiration,
+	}, nil
+}
+
+func CheckDependencies() error {
+	_, err := exec.LookPath("granted")
+	if err != nil {
+		// The executable was not found in the PATH
+		if _, ok := err.(*exec.Error); ok {
+			return clierr.New("the required cli 'granted' was not found on your path", clierr.Info("Granted is required to access AWS via SSO, please follow the instructions here to install it https://docs.commonfate.io/granted/getting-started/"))
+		}
+		return err
+	}
+	_, err = exec.LookPath("aws")
+	if err != nil {
+		// The executable was not found in the PATH
+		if _, ok := err.(*exec.Error); ok {
+			return clierr.New("the required cli 'aws' was not found on your path", clierr.Info("The AWS cli is required to access dastabases via SSM Session Manager, please follow the instructions here to install it https://docs.aws.amazon.com/cli/latest/userguide/cli-chap-welcome.html"))
+		}
+		return err
+	}
+	return nil
+}
+
+func GrantedCredentialProcess(commandData CommandData) (aws.Credentials, error) {
+
+	configFile := fmt.Sprintf(`[profile cf-cli]
+sso_session = cf-cli
+sso_account_id = %s
+sso_role_name = %s
+region = %s
+
+[sso-session cf-cli]
+sso_start_url = %s
+sso_region = %s
+`, commandData.GrantOutput.AccountID, commandData.GrantOutput.SSORoleName, commandData.GrantOutput.SSORegion, commandData.GrantOutput.SSOStartURL, commandData.GrantOutput.SSORegion)
+
+	file, err := os.CreateTemp(os.TempDir(), "")
+	if err != nil {
+		return aws.Credentials{}, err
+	}
+	defer file.Close()
+	defer os.Remove(file.Name())
+	_, err = file.Write([]byte(configFile))
+	if err != nil {
+		return aws.Credentials{}, err
+	}
+	err = file.Close()
+	if err != nil {
+		return aws.Credentials{}, err
+	}
+
+	cmd := exec.Command("granted", "credential-process", "--auto-login", "--profile", "cf-cli")
+	cmd.Env = append(cmd.Env, os.Environ()...)
+	cmd.Env = append(cmd.Env, "AWS_CONFIG_FILE="+file.Name())
+
+	var stdout strings.Builder
+	var stderr strings.Builder
+	cmd.Stdout = &stdout
+	cmd.Stderr = &stderr
+
+	err = cmd.Run()
+	clio.Debugw("granted credentials process stderr output", "stderr", stderr.String())
+	if err != nil {
+		return aws.Credentials{}, err
+	}
+	return ParseCredentialProcessOutput(stdout.String())
 }
