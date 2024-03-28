e545ca0cd8b28277fc4cfd4d9b87f1cb0ac340d4
JoshuaWilkes <14214200+JoshuaWilkes@users.noreply.github.com>
2024-03-28 11:44:58 +0800

    Add relink command

---
+++ cmd/cli/command/integration/relink_entra_users.go
@@ -0,0 +1,32 @@
+package integration
+
+import (
+	"connectrpc.com/connect"
+	"github.com/common-fate/clio"
+	"github.com/common-fate/sdk/config"
+	resetv1alpha1 "github.com/common-fate/sdk/gen/commonfate/control/integration/reset/v1alpha1"
+	"github.com/common-fate/sdk/service/control/integration/reset"
+	"github.com/urfave/cli/v2"
+)
+
+var relinkEntraUsers = cli.Command{
+	Name: "relink-entra-users",
+	Action: func(c *cli.Context) error {
+		ctx := c.Context
+
+		cfg, err := config.LoadDefault(ctx)
+		if err != nil {
+			return err
+		}
+
+		client := reset.NewFromConfig(cfg)
+
+		_, err = client.RelinkEntraUsers(ctx, connect.NewRequest(&resetv1alpha1.RelinkEntraUsersRequest{}))
+		if err != nil {
+			return err
+		}
+
+		clio.Success("relinked Entra::User entities to CF::User entities")
+		return nil
+	},
+}
