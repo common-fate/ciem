a6ee29df24ffaa2e8a8d7a580da74870ff12ca3d
JoshuaWilkes <14214200+JoshuaWilkes@users.noreply.github.com>
2024-09-13 10:07:56 +0800

    Add a closed flag to the list requests command (#93)

--- cmd/cli/command/access/list/requests.go
+++ cmd/cli/command/access/list/requests.go
@@ -23,6 +23,7 @@ var requestsCommand = cli.Command{
 	Flags: []cli.Flag{
 		&cli.StringFlag{Name: "output", Value: "table", Usage: "output format ('table', 'wide', or 'json')"},
 		&cli.BoolFlag{Name: "order-ascending", Usage: "list requests in ascending chronological order"},
+		&cli.BoolFlag{Name: "closed", Usage: "list closed requests"},
 	},
 	Action: func(c *cli.Context) error {
 		ctx := c.Context
@@ -45,6 +46,7 @@ var requestsCommand = cli.Command{
 			res, err := client.QueryAccessRequests(ctx, connect.NewRequest(&accessv1alpha1.QueryAccessRequestsRequest{
 				PageToken: pageToken,
 				Order:     grab.If(c.Bool("order-ascending"), entityv1alpha1.Order_ORDER_ASCENDING.Enum(), entityv1alpha1.Order_ORDER_DESCENDING.Enum()),
+				Archived:  c.Bool("closed"),
 			}))
 			if err != nil {
 				return err
